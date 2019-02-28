d98556060f818aeadfc6db52808adc3ecd0029ba
Tom Morris <tom@tommorris.org>
2019-02-28 09:14:07 +0000

    we need a mocking library, alas

--- mix.exs
+++ mix.exs
@@ -28,9 +28,9 @@ defmodule Elpushover.MixProject do
     [
       {:httpoison, "~> 1.5"},
       {:jason, "~> 1.1"},
-      {:ex_doc, "~> 0.19", only: [:dev, :test, :docs], runtime: false}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.19", only: [:dev, :test, :docs], runtime: false},
+      {:mock, "~> 0.1.1", only: :test},
+      {:inch_ex, github: "rrrene/inch_ex", only: [:dev, :test]},
     ]
   end
 end

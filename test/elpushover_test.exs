a215076416b58d4da0a262a7fb9814d00e5c9ef6
Tom Morris <tom.morris@futurelearn.com>
2020-05-07 10:44:38 +0100

    modify test to match reality

--- test/elpushover_test.exs
+++ test/elpushover_test.exs
@@ -4,9 +4,8 @@ defmodule ElpushoverTest do
   #doctest Elpushover
 
   test "send notification" do
-    #actual = Elpushover.notify("hello world")
     with_mock Elpushover.Api, [post: fn(url, params) -> PushoverNotificationMock.post(url, params) end, start: fn() -> PushoverNotificationMock.start() end] do
-      assert {:ok, body} = Elpushover.notify("hello world")
+      assert {:ok, body, _} = Elpushover.notify("hello world")
     end
   end
 end
